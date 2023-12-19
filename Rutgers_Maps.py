3e1da2bd088e78156722712fc45e69ce7d2c2c26
alyssaaaa22 <alyssaliou123@gmail.com>
2023-12-19 18:50:30 -0500

    help

---
+++ Rutgers_Maps.py
@@ -0,0 +1,7 @@
+# Get user input and store it in a variable
+user_input_origin = input("Enter your origin address: ")
+user_input_destination = input("Enter your destination address: ")
+
+# Display the user input
+print("This is your origin address: ", user_input_origin)
+print("This is your destination address: ", user_input_destination)
